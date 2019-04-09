697785e3a131c9b93adf60fd3ccec4e97ac58d31
Katherine Wren <kwren@outlook.com>
2019-04-09 15:40:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+  secondsleft = integer
+  while integer > 0 
+  puts "#{integer} SECOND(S)!"
+  countdown = 
+end
+puts "HAPPY NEW YEAR!"
 end
